d2beb491a622c277811f2581309821640e828356
mcrownover7 <39561811+mcrownover7@users.noreply.github.com>
2023-07-14 12:01:12 -0400

    Simple guess the number game using C# in the command line.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int winNum = randomNumber.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number between 0 and 100:  ");
+                string guess = Console.ReadLine();
+
+                int intGuess = int.Parse(guess);
+
+                if (intGuess > winNum)
+                {
+                    Console.WriteLine("To high! Guess lower...");
+                }
+                else if (intGuess < winNum)
+                {
+                    Console.WriteLine("To low! Guess higher...");
+                }
+                else if (intGuess == winNum)
+                {
+                    Console.WriteLine("You Win!!!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.Write("Press any key to finish.");
+            Console.ReadKey(true);
+        }
+    }
+}
